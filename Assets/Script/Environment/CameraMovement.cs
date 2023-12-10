11c72b1990eac54dee02fd82427b34ccddaeb53a
Adrian Mallari <a87kamui@gmail.com>
2023-12-10 00:59:32 -0500

    Added code in CameraMovement script to follow the mouse and clamp it within boundries Also updated the environment to create a path for enemies to go both sides of the map depending on where they spawn from enemy base

--- Assets/Script/Environment/CameraMovement.cs
+++ Assets/Script/Environment/CameraMovement.cs
@@ -4,16 +4,35 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float camSpeed = 60.0f;
+    [SerializeField] float boundary = 100.0f;
+    [SerializeField] Vector2Int camLimit;
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 position = transform.position;
+
+        if (Input.mousePosition.y >= Screen.height - boundary)
+        {
+            position.x -= camSpeed * Time.deltaTime;
+        }
+        if (Input.mousePosition.y <= boundary)
+        {
+            position.x += camSpeed * Time.deltaTime;
+        }
+        if (Input.mousePosition.x >= Screen.width - boundary)
+        {
+            position.z += camSpeed * Time.deltaTime;
+        }
+        if (Input.mousePosition.x <= boundary)
+        {
+            position.z -= camSpeed * Time.deltaTime;
+        }
+
+        position.x = Mathf.Clamp(position.x, 70, camLimit.x);
+        position.z = Mathf.Clamp(position.z, 60, camLimit.y);
+
+        transform.position = position;
     }
 }
